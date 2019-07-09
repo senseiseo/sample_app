0d1ed97ed40ec2e3106610401e05715f946c4c6d
ildar <1919mur@gmail.com>
2019-07-09 19:20:23 +0300

    //test user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,4 +8,29 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example1 User",
+                                            email: "user1@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }}
+    end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    log_in_as(user)
+    assert_not is_logged_in?
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert is_logged_in?
+   
+  end
 end
