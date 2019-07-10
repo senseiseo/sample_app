1f3fac8f29f8c7764374b36babdb4a0ff56fa54d
ildar <1919mur@gmail.com>
2019-07-10 11:01:48 +0300

    //create session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create 
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+
+      # Осуществить вход пользователя и перенаправление на страницу профиля.
+    else
+      # Выдать сообщение об ошибке.
+      flash.now[:danger] = 'Invalid email/password combination' # Не совсем верно!
+      render 'new'
+  end 
+end
+end
