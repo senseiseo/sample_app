2f5da4b2a30edab129b7e80092a2c4a8ebe7075d
ildar <1919mur@gmail.com>
2019-07-10 11:00:24 +0300

    //create new path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
+  get 'sessions/new'
   root 'static_pages#home'
   get 'static_pages/home'
   get 'help' => 'static_pages#help'
@@ -7,6 +8,10 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
   get 'users/new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
