a4f9ef3e0cf5b766d175c43060bb910772d9f3ee
ildar <1919mur@gmail.com>
2019-07-14 19:24:33 +0300

    //pause

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
 
       # Осуществить вход пользователя и перенаправление на страницу профиля.
@@ -15,7 +17,8 @@ class SessionsController < ApplicationController
   end 
 end
   def destroy
-    log_out
+    log_out if logged_in?
+
     redirect_to root_url
   end
 
